8e64581902a7cfd6b6f52b15980ae350f2061867
skuzi <serg.kuzmin00@mail.ru>
2018-10-11 23:58:57 +0300

    add combinators

---
+++ Main.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE FlexibleInstances #-}
+
+module Main where
+
+import Parser
+import Combinators (Result (Success, Error))
+
+runParser :: String -> IO ()
+runParser input = do
+  putStrLn input
+  print $ parse input
+  putStrLn ""
+
+instance {-# OVERLAPPING #-} Show a => Show (Maybe (Result a)) where
+  show (Just (Success tree)) = show tree
+  show (Just (Error err)) = "Syntax error: " ++ err
+  show Nothing = "Empty tree"
+
+main :: IO ()
+main = do
+  runParser "1-2-3"
+  runParser "(((9)))"
+  runParser "1*2-3/4+5"
+  runParser "!"
+  runParser "  1   +  ( (  2 +   3  ) )  "
+  runParser "123^2^3"
+  runParser "[1,2,3]"
+  runParser "[1, 2, [2 + 3, val = 123^15 + 10, [], [zero, one, two, 3, 4, 5 + 0, val_1 = 6]], lol]; same; 777"
+  runParser " f "
+  runParser "f   ;  f; f "
+  runParser "f   ;  f; f; but wrong; "
+  runParser "test ; x = 13; y = z = 42 + 6; 777"
+  runParser "[[[[[]]]]]"
